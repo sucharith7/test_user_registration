54089cd3e3941bd4ed5b55fd66326c2e535f13c7
sucharith7 <ch.sucharith09@gmail.com>
2020-04-22 22:59:08 +0530

    [sucharith] added last name

--- ValidateUser.py
+++ ValidateUser.py
@@ -7,3 +7,11 @@ class ValidateUser:
 			return "matched"
 		else:
 			return "not matched"
+
+
+	def lastName(self,Lname):
+		match=re.match(r'^[A-Z]{1}[a-z]{2,}$',Lname)
+		if match:
+			return "matched"
+		else:
+			return "not matched"
